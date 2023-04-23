28bf4595a634c5904ef85e86f5bfff514be9fded
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-04-23 11:01:19 +0100

    Create function to remove a single argument from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, arg) {
+    const argIndex = array.indexOf(arg);
+    const removedArg = array.splice(argIndex, 1);
+    return array;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
