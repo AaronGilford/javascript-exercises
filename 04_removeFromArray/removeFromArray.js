487b57149c9acc9d7c0353cb713e1b2c6efb36c4
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-04-23 11:23:29 +0100

    Rewrite code to suit multiple arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,11 @@
-const removeFromArray = function(array, arg) {
-    const argIndex = array.indexOf(arg);
-    const removedArg = array.splice(argIndex, 1);
-    return array;
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
 
 console.log(removeFromArray([1, 2, 3, 4], 3));
