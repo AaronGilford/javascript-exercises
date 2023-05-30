826544585b070f63ab45ce8a6991b6ebcacac2d5
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-05-30 11:22:29 +0100

    make function case insensitive and punctuation insensitive and run tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
+    let array = str
+        .toLowerCase()
+        .split("")
+        .filter(function(a){
+            return punctuation.indexOf(a) === -1;
+        });
+    let inverseArray = array.slice().reverse();
+    for (let i = 0; i < (array.length + 1) / 2; i++) {
+        if (array[i] !== inverseArray[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
