b3fabc8eb6407e8977b10c495cabef1f2f99c867
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-06-03 09:51:34 +0100

    create fibonacci sequence function that works with strings and rejects negative numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS";
+    }
+    let oldNumber = 0;
+    let difference;
+    let newNumber = 1;
+    for(i = 0; i < a; i++) {
+        if (i < 2) {
+            newNumber = 1;
+        } else {
+            difference = newNumber - oldNumber;
+            oldNumber = difference;
+            newNumber = +newNumber + difference;
+        }
+    }
+    return newNumber;
+    
 };
 
 // Do not edit below this line
